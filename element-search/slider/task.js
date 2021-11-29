ad6946ea346a61525e58157d660b2eefb52aae50
ivadelina <kapranova.milla@yandex.ru>
2021-11-29 21:41:58 +0300

    1

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,38 +1,32 @@
 let arrowLeft = Array.from(document.querySelectorAll("div.slider__arrow_prev"));
 let arrowRight = Array.from(document.querySelectorAll("div.slider__arrow_next"));
 
-let activeId = undefined;
+// Находим активный слайд
+let activeId;
 let sliderItems = Array.from(document.querySelectorAll("div.slider__item"));
-console.log(sliderItems)
-for(let i = 0; i < sliderItems.length; i++) {
-    if(sliderItems[i].classList.contains('slider__item_active')) {
-        activeId = i
+    for(let i = 0; i < sliderItems.length; i++) {
+        if(sliderItems[i].classList.contains('slider__item_active')) {
+            activeId = i
+        };
     };
-};
 
 arrowRight[0].onclick = function() {
-    console.log(activeId)
     sliderItems[activeId].classList.remove('slider__item_active');
     sliderItems[activeId++].classList.add('slider__item_active');
-    console.log(activeId)
-    if(activeId === 5) {
-        console.log(activeId)
+    if(activeId === 4) { // при id = 4 не показывается при клике последний слайд. А при 5 не может быть свойства у underfined
         sliderItems[activeId].classList.remove('slider__item_active');
-        sliderItems[0].classList.add('slider__item_active');   
-        console.log(activeId) 
+        activeId = 0;
+        sliderItems[activeId].classList.add('slider__item_active');   
     };
 };
 
 arrowLeft[0].onclick = function() {
+    sliderItems[activeId].classList.remove('slider__item_active');
+    sliderItems[activeId--].classList.add('slider__item_active');
     if(activeId === 0) {
         sliderItems[activeId].classList.remove('slider__item_active');
         activeId = 4;
         sliderItems[activeId].classList.add('slider__item_active');
-    } else if (activeId > 0) {
-        console.log(sliderItems[activeId])
-    sliderItems[activeId].classList.remove('slider__item_active');
-    sliderItems[activeId--].classList.add('slider__item_active');
-    };
-    
+    };   
 };
 
