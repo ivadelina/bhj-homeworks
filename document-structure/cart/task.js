95a33e8f4c057cb83e73c81b683a713b28d9f882
ivadelina <kapranova.milla@yandex.ru>
2022-01-15 20:49:49 +0300

    Третья задача с комментариями

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -20,12 +20,17 @@ let buttons = document.querySelectorAll('.product__add');
 buttons.forEach(function(el) {
     el.addEventListener("click", addFunc);
   });
+let cart = document.querySelector(".cart__products");  
 function addFunc(event) {
     productId = event.target.closest(".product").getAttribute('data-id');
     let shoppingCart = Array.from(document.querySelectorAll(".cart__product"));
     let findProduct = shoppingCart.filter(function(product) {
         product.getAttribute('data-id') === productId;
+        console.log(product.getAttribute('data-id') === productId) // true
       });
+      console.log(shoppingCart)
+      console.log(findProduct) // почему тут полчучается пустой массив?
+      
     if(findProduct.length === 0) {
         console.log("Надо добавить продукт в корзину");
         cart.insertAdjacentHTML('afterBegin','<div class="cart__product" data-id="1"><img class="cart__product-image" src="image.png"><div class="cart__product-count">20</div></div>'); 
@@ -33,14 +38,17 @@ function addFunc(event) {
         id = productId;
         let number = document.querySelector(".cart__product-count").innerText;
         number = event.target.previousElementSibling.children[1].innerText;
-        console.log(number)
+        console.log(number) // Почему-то всё правильно находит, но в корзине остается количество по умолчанию
         let image = document.querySelector(".cart__product-image").src;
-        // Тут надо найти ссылку на изображение товара, который добавляем
+        // Как достать до ссылки на картинку добавляемого объекта?()
 
-    } else console.log("Продукт уже в корзине")
+    } else {
+      console.log("Продукт уже в корзине")
+    }
 
 }
-let cart = document.querySelector(".cart__products");
+
+
 
 
 
