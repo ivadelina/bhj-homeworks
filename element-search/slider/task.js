561c2c9e8810d3a6a5c13db05841a935211e170b
ivadelina <kapranova.milla@yandex.ru>
2022-01-26 13:13:01 +0300

    Слайдер переделала, но не работает повторный поиск активного изображения

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,75 +2,71 @@ let items = Array.from(document.querySelectorAll(".slider__item"));
 let arrowLeft = document.querySelector(".slider__arrow_prev");
 let arrowRight = document.querySelector(".slider__arrow_next");
 let dots = Array.from(document.querySelectorAll(".slider__dot"));
-
-let activeId = items.findIndex(
-    function active() {
-        for (el of items) {
-            for (let el of items) {
-                if(el.matches(".slider__item_active")) {
-                    return true
+let lastElId = items.length - 1
+function beActive(currentActiveId) {
+    let activeId = items.findIndex(
+        function active() {
+            for (el of items) {
+                for (let el of items) {
+                    if(el.matches(".slider__item_active")) {
+                        return true
+                    };
+                    return false    
+                };
             };
-        return false    
-        };
+        });
+    console.log(activeId) 
+    console.log(currentActiveId)   
+    items[activeId].classList.remove("slider__item_active");
+    dots[activeId].classList.remove("slider__dot_active");
+    if(currentActiveId < 0) {
+        items[lastElId].classList.add("slider__item_active");
+        dots[lastElId].classList.add("slider__dot_active");
+    } else if(currentActiveId > lastElId) {
+        items[0].classList.add("slider__item_active");
+        dots[0].classList.add("slider__dot_active");
+    } else {
+        items[currentActiveId].classList.add("slider__item_active");
+        dots[currentActiveId].classList.add("slider__dot_active");
     };
+};
+arrowRight.onclick = function() {
+    let activeId = items.findIndex(
+        function active() {
+            for (el of items) {
+                for (let el of items) {
+                    if(el.matches(".slider__item_active")) {
+                        return true
+                    };
+                    return false    
+                };
+            };
+        });
+    console.log(activeId)  
+    let newActive = activeId + 1; 
+    console.log(newActive) 
+    beActive(newActive);
+};
+arrowLeft.onclick = function() {
+    let activeId = items.findIndex(
+        function active() {
+            for (el of items) {
+                for (let el of items) {
+                    if(el.matches(".slider__item_active")) {
+                        return true
+                    };
+                    return false    
+                };
+            };
+        });
+    console.log(activeId)    
+    beActive(activeId - 1);
+};  
+dots.forEach(function(dot, id) {
+    dot.addEventListener("click", function() {
+        beActive(id);
     });
- 
- /*    // Функция установки активного изображения 
-    function beActive(obj,id) {
-        console.log(id)
-        obj[id].classList.remove("slider__item_active")
-        id++ // почему это значение не перезаписывается здесь, но точно по такой же схеме перезаписывается в рабосчем коде?
-        console.log(id)
-        obj[id].classList.add("slider__item_active")
-    };
-    arrowRight.onclick = function() {
-        activeId === 4 ? (
-            items[activeId].classList.remove("slider__item_active"),
-            dots[activeId].classList.remove("slider__dot_active"),
-            activeId = 0,
-            items[activeId].classList.add("slider__item_active"),
-            dots[activeId].classList.add("slider__dot_active") 
-            ) : ( beActive(items,activeId)
-            );
-    }; */
- 
-    arrowRight.onclick = function() {
-        activeId === 4 ? (
-            items[activeId].classList.remove("slider__item_active"),
-            dots[activeId].classList.remove("slider__dot_active"),
-            activeId = 0,
-            items[activeId].classList.add("slider__item_active"),
-            dots[activeId].classList.add("slider__dot_active") 
-            ) : ( 
-            items[activeId].classList.remove("slider__item_active"),
-            dots[activeId].classList.remove("slider__dot_active"),
-            activeId++,
-            items[activeId].classList.add("slider__item_active"),
-            dots[activeId].classList.add("slider__dot_active"));
-    };
-    arrowLeft.onclick = function() {
-        activeId === 0 ? (
-            items[activeId].classList.remove("slider__item_active"),
-            dots[activeId].classList.remove("slider__dot_active"),
-            activeId = 4,
-            items[activeId].classList.add("slider__item_active"),
-            dots[activeId].classList.add("slider__dot_active")
-            ) : (           
-            items[activeId].classList.remove("slider__item_active"),
-            dots[activeId].classList.remove("slider__dot_active"),
-            activeId--,
-            items[activeId].classList.add("slider__item_active"),
-            dots[activeId].classList.add("slider__dot_active"));
-    };  
-    for(let el of dots) {
-        el.onclick = function() {
-            items[activeId].classList.remove("slider__item_active");
-            dots[activeId].classList.remove("slider__dot_active");
-            activeId = dots.indexOf(el);
-            items[activeId].classList.add("slider__item_active");
-            dots[activeId].classList.add("slider__dot_active");
-        };
-    };
+});
 
 
 
