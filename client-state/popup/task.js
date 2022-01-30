9a29d50576e347d1ee7508cc065265b5e96f5b8f
ivadelina <kapranova.milla@yandex.ru>
2022-01-30 21:43:09 +0300

    Client-state 1,2 done, auth -

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,25 +1,23 @@
-
-document.cookie = "wasClosed=true; SameSite=None; Secure";
-
-    if (!document.cookie) {
-      return null;
-    }
-    let cookies = document.cookie.split('; ');
-    console.log(cookies);
-    let cookie = cookies.find((c) => c.startsWith("wasClosed" + '=')) || '';
-    console.log(cookie)
-    const value = decodeURIComponent(cookie.substr(key.length + 1));
-    return (cookie) ? value : null;
-  
-
-
-
-let sibscribeBox = document.getElementById("subscribe-modal");
-setTimeout(() => sibscribeBox.classList.add("modal_active"), 3000);
-document.querySelector(".modal__close").addEventListener("click", () => {
-    sibscribeBox.classList.remove("modal_active");
-    document.cookie = "wasClosed=true; SameSite=None; Secure";
-
-});
+function setCookie(key, value) {
+  document.cookie = key + '=' + encodeURIComponent(value);
+  return true;
+};
+let check = (key) => {
+  let value = "; " + document.cookie;
+  let parts = value.split("; "+ key + "=");
+  if (parts.length=== 2) { 
+    return true
+  };
+};
+check("wasClosed");
+if(!check) {
+  let sibscribeBox = document.getElementById("subscribe-modal");
+  setTimeout(() => sibscribeBox.classList.add("modal_active"), 3000);
+  document.querySelector(".modal__close").addEventListener("click", () => {
+  sibscribeBox.classList.remove("modal_active");
+    /* document.cookie = "wasClosed=true; SameSite=None; Secure"; */
+    setCookie("wasClosed", true)
+  });
+};
 
 
