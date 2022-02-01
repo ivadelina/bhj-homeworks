99863a57a9731df111ef3b68b83eebb43b29e1af
ivadelina <kapranova.milla@yandex.ru>
2022-02-01 20:31:33 +0300

    progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,13 +1,13 @@
-let xhr = new XMLHttpRequest();
-xhr.open('POST','https://netology-slow-rest.herokuapp.com/upload.php');
+let formData = new FormData(document.getElementById("form"));
 let progress = document.getElementById('progress');
-document.querySelector("#form").addEventListener("submit", function(e){
-  e.preventDefault(); 
-  let formData = new FormData(document.getElementById("form"))
-  xhr.send(formData); // Тут выдает ошибку, что The object's state must be OPENED.
-  xhr.upload.onprogress = function(event) {
-    // здесь, видимо, должно быть если event.progress, то progress.value = progress.value + 0,2;
-  // В примерах везде консоль, а если мне нужно написать условие, то писать if(event.progress){}?
-  };
+document.querySelector("#form").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  let xhr = new XMLHttpRequest();
+  xhr.upload.onloadstart = () => progress.value = 0.1;
+  xhr.upload.onprogress = () => progress.value = progress.value + 0.2;
+  xhr.upload.onload = () => progress.value = 1;
+  xhr.open('POST','https://netology-slow-rest.herokuapp.com/upload.php');
+  xhr.send(formData);
 });
+
 
