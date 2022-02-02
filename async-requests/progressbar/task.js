dabb4ab0b3b64bdf274927407ad3c700e176eec3
ivadelina <kapranova.milla@yandex.ru>
2022-02-02 20:56:17 +0300

    No progress

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -3,11 +3,17 @@ let progress = document.getElementById('progress');
 document.querySelector("#form").addEventListener("submit", function(event) {
   event.preventDefault(); 
   let xhr = new XMLHttpRequest();
-  xhr.upload.onloadstart = () => progress.value = 0.1;
-  xhr.upload.onprogress = () => progress.value = progress.value + 0.2;
-  xhr.upload.onload = () => progress.value = 1;
+  xhr.upload.onloadstart = (event) => progress.value = 0.1;
+  xhr.upload.onprogress = (event) => {
+    if (event.lengthComputable) {
+      console.log(event)
+      progress.value = progress.value + 0.2;
+    };
+  };
+  xhr.upload.onload = (event) => progress.value = 1;
   xhr.open('POST','https://netology-slow-rest.herokuapp.com/upload.php');
   xhr.send(formData);
 });
+
 
 
